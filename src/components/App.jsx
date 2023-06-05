3e3b65334bb37633ed565cfa1a9e8487e62e3c13
Waldemar Kubiak <kubiak.waldemar@gmail.com>
2023-06-05 22:53:03 +0200

    ContactForm version one

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,37 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { ContactForm } from './ContactForm/ContactForm';
+import { nanoid } from 'nanoid';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  addContact = ({ name, number }) => {
+    const isItUniqueName = this.state.contacts
+      .map(e => e.name.toLowerCase())
+      .includes(name.toLowerCase());
+    if (isItUniqueName) {
+      return alert(`${name} is already in contacts.`);
+    } else {
+      const contact = {
+        id: nanoid(),
+        name: name,
+        number: number,
+      };
+      this.setState(prevState => ({
+        contacts: [...prevState.contacts, contact],
+      }));
+    }
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <h1>Phonebook</h1>
+        <ContactForm onAddContact={this.addContact} />
+      </div>
+    );
+  }
+}
